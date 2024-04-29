f29093d40a7d13836cf4be0c4eb8166e2d958485
RichesUR7 <richesuche91@gmail.com>
2024-04-29 23:40:33 +0000

    RESTful

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,41 +1,49 @@
 #!/usr/bin/python3
-"""Create a new Flask app"""
-from os import environ
-
-from flask import Flask, jsonify, make_response
-from flask_cors import CORS
-
-from api.v1.views import app_views
+""" Flask Application """
 from models import storage
+from api.v1.views import app_views
+from os import environ
+from flask import Flask, render_template, make_response, jsonify
+from flask_cors import CORS
+from flasgger import Swagger
+from flasgger.utils import swag_from
 
 app = Flask(__name__)
-# CORS instance allowing all origins
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
-
-app.config["JSONIFY_PRETTYPRINT_REGULAR"] = True
-app.config['JSON_SORT_KEYS'] = True
-app.url_map.strict_slashes = False
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def teardown_appcontext(self):
-    """Teardown appcontext"""
+def close_db(error):
+    """ Close Storage """
     storage.close()
 
 
 @app.errorhandler(404)
 def not_found(error):
-    """Handler for 404 errors"""
-    return make_response(jsonify({"error": "Not found"}), 404)
+    """ 404 Error
+    ---
+    responses:
+      404:
+        description: a resource was not found
+    """
+    return make_response(jsonify({'error': "Not found"}), 404)
+
+app.config['SWAGGER'] = {
+    'title': 'AirBnB clone Restful API',
+    'uiversion': 3
+}
+
+Swagger(app)
 
 
 if __name__ == "__main__":
-    host = environ.get("HBNB_API_HOST")
-    port = environ.get("HBNB_API_PORT")
+    """ Main Function """
+    host = environ.get('HBNB_API_HOST')
+    port = environ.get('HBNB_API_PORT')
     if not host:
-        host = "0.0.0.0"
-
+        host = '0.0.0.0'
     if not port:
-        port = "5000"
+        port = '5000'
     app.run(host=host, port=port, threaded=True)
